bf65a6f6dea0a99eac5752567ea7b3590fd9967b
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-02 16:56:21 +0530

    Implement method to find empty cell in the board

--- sudoku solver.py
+++ sudoku solver.py
@@ -28,3 +28,12 @@ class Board:
                 board_string += lower_lines
 
         return board_string
+    
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
